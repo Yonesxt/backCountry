8e315aceea7deb1d928ce61b2caa5b1130b4fb25
Yonesxt <Yones.rpy@gmail.com>
2022-08-17 04:20:54 -0300

    back

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port= process.env.PORT || 3001;
 // Syncing all the models at once.
 const {  Country } = conn.models;
 conn.sync({ force: false }).then(() => {
-  server.listen(port, () => {
+  server.listen(port,async () => {
     const country = await Country.findAll()
     if(!country.length){ 
     try {
